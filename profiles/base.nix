0faa1b2d2d7fe6f2484809e271e005009efda014
overflowdo <overflodo@web.de>
2026-06-12 15:47:11 +0200

    gdf

--- profiles/base.nix
+++ profiles/base.nix
@@ -11,11 +11,7 @@
     };
 
     security.sudo.wheelNeedsPassword = true;
-    
-    environment.etc."scripts/export_xpub" = {
-        source = ./files/export_xpub.sh;
-        mode = "0755";
-    };
+
 
     environment.etc."scripts/wgHMAC_exporth" = {
         source = ./files/wgHMAC_export.sh;
@@ -24,10 +20,6 @@
 
 
     #Wrappers
-    environment.etc."scripts/wrappers/export_xpub.sh" = {
-        source = ./files/wrappers/export_xpub.sh;
-        mode = "0755";
-    };
 
     environment.etc."scripts/wrappers/wgHMAC_export.sh" = {
         source = ./files/wrappers/wgHMAC_export.sh;
@@ -42,7 +34,6 @@
         "d /home/user/Desktop 0750 user users - -"
         "d /home/user/Desktop/scripts 0750 user users - -"
 
-        "L+ /home/user/Desktop/scripts/export_xpub.sh - - - - /etc/scripts/wrappers/export_xpub.sh"
         "L+ /home/user/Desktop/scripts/wgHMAC_export.sh - - - - /etc/scripts/wrappers/wgHMAC_export.sh"
     ];
 
